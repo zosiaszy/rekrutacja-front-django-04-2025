9fe7dd6c70a471a481a95d38c623ba7cd91aa4cb
zosiaszyp@gmail.com <zosiaszyp@gmail.com>
2025-04-06 17:42:31 +0200

    Add changes for index, base and event_detail

--- event_manager/urls.py
+++ event_manager/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('events.urls')),
+   path('', include('events.urls', namespace='events')), 
+
 ]
 
 if settings.DEBUG:
--- events/urls.py
+++ events/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "events" 
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('events/create/', views.EventCreateView.as_view(), name='event_create'),
@@ -8,4 +10,4 @@ urlpatterns = [
     path('events/<int:pk>/edit/', views.EventUpdateView.as_view(), name='event_update'),
     path('events/monthly/', views.monthly_events, name='monthly_events'),
     path('events/monthly/<int:year>/<int:month>/', views.monthly_events, name='monthly_events_date'),
-] 
+]
